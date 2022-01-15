b31f66c4d168d4a7f13fb7b601c515a8a1b2b20c
ahmadnadeem123 <ahmadnadeempucit@gmail.com>
2022-01-15 19:27:57 +0500

    Fragment Completed

--- Fragment/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Fragment/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b1;
+    Button b2;
+    LinearLayout myLayout;
+    LinearLayout myLayout2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        b1=findViewById(R.id.button);
+        b2=findViewById(R.id.button2);
+
+//        myLayout=findViewById(R.id.linearLayout);
+//        myLayout2=findViewById(R.id.linearlayout2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BlankFragment fragment1 = new BlankFragment();
+                FragmentTransaction transaction =getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.linearLayout, fragment1);
+                transaction.commit();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BlankFragment fragment1 = new BlankFragment();
+                FragmentTransaction transaction =getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.linearlayout2, fragment1);
+                transaction.commit();
+            }
+        });
+
     }
 }
